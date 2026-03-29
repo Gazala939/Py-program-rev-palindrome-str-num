5053f7226520c90722dbeed2b7f62ad8b466c18b
Gazala939 <Gazalajamadar786@gmail.com>
2026-03-29 11:42:35 +0530

    reverse and palindrome of number

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ rev = " "
 
 for i in s:
     rev = i + rev
-print("Reverse String: ", rev)
+print("Reverse String: ", rev) # Reverse String:  nohtyP 
 
 
-# Check Palindrome:
+# Check Palindrome of String:
 
 s = "level"
 rev = ""
@@ -19,4 +19,32 @@ for i in s:
 if s == rev:
     print(f"{s} is a Palindrome")
 else:
-    print(f"{s} is not a palindrome")
+    print(f"{s} is not a palindrome") # level is a Palindrome
+
+
+# Reverse a Number:
+
+num = 1234
+rev = 0
+
+while num > 0:
+    digit = num % 10   # gry the last digit
+    rev = rev * 10 + digit
+    num = num // 10 # Remove the last digit
+print("Reversed number: ", rev)
+
+# Check Palindrome of number
+
+num = 121
+rev = 0
+original = num
+
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num = num // 10
+
+if original == rev:
+    print(f"{original} is a Palindrome") 
+else:
+    print(f"{original} is not a Palindrome")
