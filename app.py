12df6b96320229d3cc1cccabeca71e95696f7649
Gazala939 <Gazalajamadar786@gmail.com>
2026-03-29 11:32:49 +0530

    Check Palindrome of String

--- app.py
+++ app.py
@@ -8,4 +8,15 @@ for i in s:
 print("Reverse String: ", rev)
 
 
+# Check Palindrome:
 
+s = "level"
+rev = ""
+
+for i in s:
+    rev = i + rev
+
+if s == rev:
+    print(f"{s} is a Palindrome")
+else:
+    print(f"{s} is not a palindrome")
